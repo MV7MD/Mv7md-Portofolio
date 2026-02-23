9883444f11826b5f6575536bb451b05105edd43d
Muhammad Abuelmagd <mv7mdvboelmagd@gmail.com>
2026-02-23 20:52:54 +0200

    Delete old Back End folder

--- Back End/models/Visit.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const visitSchema = new mongoose.Schema({
-    id: { type: String, default: "main_counter" },
-    count: { type: Number, default: 0 }
-});
-
-module.exports = mongoose.model('Visit', visitSchema);
